b77a2c7a8f6ecb06630ee48df09f365503d22938
kho903 <gmldnr2222@naver.com>
2020-07-26 15:31:15 +0900

    CRUD URL 지정

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
+from bookmark import views
 from bookmark.views import BookmarkLV, BookmarkDV
 
 app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkLV.as_view(), name='index'),
     path('<int:pk>', BookmarkDV.as_view(), name='detail'),
+
+    # /bookmark/add/
+    path('add/', views.BookmarkCreateView.as_view(), name='add'),
+    path('change/', views.BookmarkChangeLV.as_view(), name='change'),
+    path('<int:pk>/update/', views.BookmarkUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.BookmarkDeleteView.as_view(), name='delete'),
 ]
